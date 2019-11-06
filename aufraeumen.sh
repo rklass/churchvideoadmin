1c69b93fcb798fc960e704b4bd27e87186644317
rklass <reinhold.klass@onlinehome.de>
2019-11-06 11:49:57 +0100

    Add files via upload

--- aufraeumen.sh
+++ aufraeumen.sh
@@ -6,6 +6,16 @@ echo "     Paramter 1 = " $1 " (aus Programm aufraeumen.sh)"
 echo " "
 echo " "
 echo "-------------------------------------------------------"
+echo "pruefen, vieviele Files im videoarchiv sind"
+anzf=$(find /video_recordings/$1/ -maxdepth 1 -type f | wc -l)
+echo "Anzahle Files = "  $anzf
+if [ $anzf -lt 7 ]
+then
+   echo "Nichts machen, weil nur $anzf Files drin sind"
+   exit
+fi
+echo "Es geht weiter"
+echo "-------------------------------------------------------"
 echo "B: Jetzt startet/home/rarents/delkruschtX.sh $1"
 bash /home/rarents/delkruschtX.sh $1
 echo  "delkruschtX.sh ist mit Parameter=" $1 " gelaufen. Exit=" $?
